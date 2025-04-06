c26d35162d05a4aab8658bb6c7ebd6aed2e6a4b4
dane-stevens <dane@dsmedia.ca>
2025-04-06 07:31:03 -0400

    first

---
+++ app/routes/_index.tsx
@@ -0,0 +1,23 @@
+
+export function meta() {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+  <div className='max-w-(--breakpoint-lg) mx-auto p-8'>
+      <h1 className="text-4xl text-center">Go to the Ant</h1>
+      <quote>Go the the ant, O sluggarg; consider her ways, and be wise. Without having any chief, officer, or ruler, she prepares her bread in summer and gathers her food in harvest.<cite>Proverbs 6:6-8</cite></quote>
+      <h2 className="text-xl">Priorities</h2>
+      <ol className="list-decimal ml-8">
+        <li>
+          
+        </li>
+
+      </ol>
+    </div>
+  )
+}
